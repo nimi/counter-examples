535160cca0f1ba31439d278f2264ab4feb53bc65
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2016-02-26 09:06:41 -0500

    Add comments to cycle

--- examples/cycle/app.js
+++ examples/cycle/app.js
@@ -2,11 +2,16 @@ const {Observable} = Rx;
 const {div, button, p, makeDOMDriver} = CycleDOM;
 
 function main({DOM}) {
+	// Intent
 	let action$ = Observable.merge(
 		DOM.select('.decrement').events('click').map(ev => -1),
 		DOM.select('.increment').events('click').map(ev => +1)
 	);
+	
+	// Model
 	let count$ = action$.startWith(0).scan((x,y) => x+y);
+	
+	// View
 	return {
 		DOM: count$.map(count =>
 			div([
